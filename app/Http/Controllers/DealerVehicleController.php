f2b61aabf0780b76bc05525d65de8eab5634b356
sundew28 <chickuthomasgeorge@gmail.com>
2022-11-21 12:03:24 +0000

    addition of repository paths to controller

--- app/Http/Controllers/DealerVehicleController.php
+++ app/Http/Controllers/DealerVehicleController.php
@@ -10,13 +10,33 @@ use App\Repository\VehiclesRepositoryInterface;
 /*
 | The purpose of this class DealerVehicleController is to set a single entry point for creating and processing records for dealers 
 | and vehicles and making API calls.
-| The repository pattern design is used here as example to show case.
+| The repository pattern design is used here as example to show case keeping in mind the S.O.L.I.D principles
 | You create the required class interface and register them with the RepositoryServiceProvider
 */
 
 class DealerVehicleController extends Controller
 {
+    /**
+     * @var private $dealersRepository
+     */
+    private $dealersRepository;
+    
+    /**
+     * @var private $vehiclesRepository
+     */
+    private $vehiclesRepository;
 
-
+   /**
+    * The Dependency Inversion Principle
+    * 
+    * @param  DealersRepositoryInterface  $dealersRepository
+    * 
+    * @param  VehiclesRepositoryInterface  $vehiclesRepository
+    */
+   public function __construct(DealersRepositoryInterface $dealersRepository, VehiclesRepositoryInterface $vehiclesRepository)
+   {
+      $this->dealersRepository = $dealersRepository;
+      $this->vehiclesRepository = $vehiclesRepository;
+   }
    
 }
